117866536ab1bdf43aa083ce50eecbd8c6a5410b
Luffy <120936732+anime91eki@users.noreply.github.com>
2023-05-01 14:06:15 +0530

    Update getid.py

--- plugins/getid.py
+++ plugins/getid.py
@@ -46,6 +46,6 @@ def get_file_id(msg: Message):
             "voice",
             "sticker",
         ):
-            if obj := getattr(msg, message_type):
-                setattr(obj, "message_type", message_type)
-                return obj
+     if obj := getattr(msg, message_type):
+            setattr(obj, "message_type", message_type)
+            return obj
