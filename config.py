a1e114cebdce47fc35711be50426df9e6ac9c34b
Otaku4fun <141166008+Otaku4fun@users.noreply.github.com>
2023-08-02 16:07:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
 UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 
 # Database
-DB_URI = os.environ.get("DATABASE_URL", "postgres://csgntwtz:o_PzyaBIMB5z_LYM76VSind6P5tfJ4FI@horton.db.elephantsql.com/csgntwtz")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://mickey007p:Mickey1234@cluster0.2atcxdp.mongodb.net")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001977716290"))
